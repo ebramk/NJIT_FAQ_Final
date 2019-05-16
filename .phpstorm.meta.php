e023e6a5415e56156a52ff577874e8b37a9a0974
ebramk <er86@njit.edu>
2019-05-16 16:08:05 -0400

    undo changes

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-05-09 01:11:25.
+    * Generated on 2019-05-16 19:52:30.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
